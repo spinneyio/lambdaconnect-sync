d81a66d2184210f34ff5080bd0df38ebbb0e7c47
Marek Lipert <marek@spinney.io>
2024-07-23 14:32:26 +0200

    Remove AOT

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Synchronisation library. <br> <br>
 
 Leiningen coordinates:
 ```clojure
-[io.spinney/lambdaconnect-sync "1.0.25"]
+[io.spinney/lambdaconnect-sync "1.0.26"]
 ```
 
 ## Usage
--- project.clj
+++ project.clj
@@ -1,14 +1,13 @@
-(defproject io.spinney/lambdaconnect-sync "1.0.25"
+(defproject io.spinney/lambdaconnect-sync "1.0.26"
   :description "Synchronisation library"
   :url "https://github.com/spinneyio/lambdaconnect-sync"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.3"]
                  [org.clojure/algo.generic "0.1.3"]
-                 [io.spinney/lambdaconnect-model "1.0.19"]]
-  :main lambdaconnect-sync.core
+                 [io.spinney/lambdaconnect-model "1.0.20"]]
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
+  :profiles {:uberjar {:aot :none
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
              :dev           [:project/dev]
              :test          [:project/dev :project/test]
